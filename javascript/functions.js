d68e2ed9873631b8f34cd76b9fed3f2e41cfaa3e
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-03 13:39:57 -0500

    Functions in JavaScript

--- javascript/functions.js
+++ javascript/functions.js
@@ -13,3 +13,33 @@ const print = function(a,b){
 const result = print(7,8);
 console.log(result);
 print(9);
+
+// object with method
+const rectangle = {
+    width = 10,
+    height = 20,
+    area: function(){
+      return this.width * this.height;
+    }
+};
+
+// method call
+console.log(rectangle.area());
+
+// constructor defintion
+const Rectangle = function(width, height){
+  this.width = width;
+  this.height = height;
+}
+// shared method
+Ractangle.prototype.area = function(){
+  return this.width * this.height;
+}
+
+// constructing objs
+const small = new Rectangle(1,2);
+const large = new Rectangle(10,20);
+
+// calls
+console.log(small.area());
+console.log(large.area());
