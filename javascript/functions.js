9161498ba6d759086c0e755f69698bd1c0ba5a32
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-03 13:24:21 -0500

    Javaxcript functions

---
+++ javascript/functions.js
@@ -0,0 +1,15 @@
+// Function syntax
+
+// defintion
+const avg = fucntion(a,b){
+  return (a+b)/2;
+};
+
+const print = function(a,b){
+  console.log(`a=${a}, b=${b}`);
+};
+
+// undefined values
+const result = print(7,8);
+console.log(result);
+print(9);
