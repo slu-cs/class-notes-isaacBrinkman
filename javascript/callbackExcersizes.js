260ad1241e4ba3acd5e9d6acd38082d870a9feb4
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-05 14:13:52 -0500

    Q1

---
+++ javascript/callbackExcersizes.js
@@ -0,0 +1,11 @@
+//Q1
+const filter = function(n, f(num){
+  const copy =[]
+  for(const a in n){
+    copy.push(f(n));
+  }
+  return copy
+});
+
+const numbers = [1, 2, 3, 4, 5];
+const odds = filter(numbers, x => x%2 > 0); 
