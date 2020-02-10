1fbe96aa3a6657a5915cedf247c89d0bf4ad2764
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-10 13:28:40 -0500

    MongoDB schema

---
+++ Mongo/faculty/schema.js
@@ -0,0 +1,20 @@
+// Define a plan for a collection
+
+const mongoose = require('mongoose');
+
+// Schema for a collection of professors
+const Professor = new mongoose.Schema({
+  name: String,
+  rank: String,
+  started: Number,
+  courses: [Number]
+});
+
+// Speed up queries on all fields
+Proffesor.index({name: 1});
+Proffesor.index({rank: 1});
+Proffesor.index({started: 1});
+Proffesor.index({courses: 1});
+
+// Compile and export this schema
+module.exports = mongoose.model('Professor', Professor);;;;;;;
