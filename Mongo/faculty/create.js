db8baf3372c4ed889aeb08fd20baf5e19a5cfc95
Isaac Brinkman <ibrin16@stlawu.edu>
2020-02-12 13:45:33 -0500

    Prmises

--- Mongo/faculty/create.js
+++ Mongo/faculty/create.js
@@ -4,35 +4,35 @@ const mongoose = require('mongoose');
 const connect = require('./db');
 const Professor = require('./schema');
 
-connect(); // connect to database
+connect(); // To the database
 
 // Create some faculty
 const harcourt = new Professor({
   name: 'Ed Harcourt',
   rank: 'Full',
   started: 2003,
-  courses: [140,220,345,362,364]
+  courses: [140, 220, 345, 362, 364]
 });
 
 const torrey = new Professor({
   name: 'Lisa Torrey',
   rank: 'Associate',
   started: 2009,
-  courses: [140,219,332,362,374,380]
+  courses: [140, 219, 332, 362, 374, 380]
 });
 
 const lee = new Professor({
   name: 'Choong-Soo Lee',
   rank: 'Associate',
   started: 2010,
-  courses: [140,219,256,321,370]
+  courses: [140, 219, 256, 321, 370]
 });
 
-// delete any previous data
+// Delete any previous data
 mongoose.connection.dropDatabase()
   .then(() => harcourt.save())
   .then(() => torrey.save())
-  .then(() =>lee.save())
-  .then(() => mongoose.connection.close()
-  .then(() => console.log("DB is ready"))
-  .catch(error => console.log(error.stack));
+  .then(() => lee.save())
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
