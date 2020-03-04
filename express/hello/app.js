abdcc425a39f5f0d2fcbf3a418e99adbe035b2e2
Isaac Brinkman <ibrin16@stlawu.edu>
2020-03-04 13:35:25 -0500

    Express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -6,9 +6,31 @@ const express = require('express');
 const app = express();
 
 // Respond to a single request
+// cs-linuxlab-07.stlawu.edu:3000/
 app.get('/', function(request, response){
   response.send('Hello Isaac');
 });
+
+// cs-linuxlab-07.stlawu.edu:3000/foo
+app.get('/foo', function(request, response){
+  response.send('Hello foo');
+});
+
+// cs-linuxlab-07.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response){
+  response.send('Hello bar');
+});
+
+// cs-linuxlab-07.stlawu.edu:3000/zar/z (for any z)
+app.get('/zap/:z', function(request, response){
+  response.send(`Hello ${request.params.z}`);
+});
+
+// cs-linuxlab-07.stlawu.edu:3000/zap?x=X&y=Y
+app.get('/zap', function(request, response){
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
+});
+
 
 // Start the server
 app.listen(3000);
