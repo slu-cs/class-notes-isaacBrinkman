aaf0f7b1f67cf7022d575459a8b54e9e3d153aef
Isaac Brinkman <ibrin16@stlawu.edu>
2020-03-09 14:10:28 -0400

    EJs for pies

--- express/bakery/pies.js
+++ express/bakery/pies.js
@@ -1,28 +1,29 @@
-// Router for pie-related requests
+// Router for cake-related requests
 const express = require('express');
 
 // Create the router
 const router = express.Router();
 
-// cs-linuxlab-##.stlawu.edu:3000/pies/
+// Pretend database collection
+const pies= [
+  {id: 'apple', flavor:'Apple'},
+  {id: 'cherry', flavor:'Cherry'}
+
+]
+
+// cs-linuxlab-##.stlawu.edu:3000/cakes/
 router.get('/', function(request, response) {
-  response.send(`
-    <h1>Pies</h1>
-    <ul>
-      <li><a href="/pies/apple">Apple</a></li>
-      <li><a href="/pies/cherry">Cherry</a></li>
-    </ul>
-  `);
+  response.render('pies/index', {pies: piespie});
 });
 
-// cs-linuxlab-##.stlawu.edu:3000/pies/id
+// cs-linuxlab-##.stlawu.edu:3000/cakes/id
 router.get('/:id', function(request, response, next) {
-  if (request.params.id === 'apple') {
-    response.send('Apple pies');
-  } else if (request.params.id === 'cherry') {
-    response.send('Cherry pies');
-  } else {
-    next(); // Pass on this request
+  // Pretend database lookup
+  const pie = pies.find(cake => pie.id === request.params.id);
+  if(!pie) {
+    next(); // Pass on the request
+  } else{
+    response.render('pies/detail', {pie: pie});
   }
 });
 
