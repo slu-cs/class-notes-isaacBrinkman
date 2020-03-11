9a24872bc22e0fbe6bc1259106d3fb807fb243c4
Isaac Brinkman <ibrin16@stlawu.edu>
2020-03-11 14:12:30 -0400

    Query strings for filtering

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -6,65 +6,68 @@ const router = express.Router();
 
 // Pretend database
 let flowers = [
-  {id: 'tulip', color: 'pink', season:'spring'},
-  {id: 'rose', color: 'red', season:'summer'},
+  {id: 'tulip', color: 'pink', season: 'spring'},
+  {id: 'rose', color: 'red', season: 'summer'}
 ];
 
 // GET /flowers
-router.get('/', function(request, response){
+router.get('/', function(request, response) {
   response.send(flowers);
-});
-// GET /flowers/id
-router.get('/:id', function(request, response, next){
-  const flower = flowers.find(f => f.id === request.params.id);
-  if(!flower){
-    next();
+  if(request.query.color){
+    response.send(flowers.filter(f => request.query.color === f.color))
+  }
+  else if(request.query.season){
+      response.send(flowers.filter(f => request.query.season === f.season))
   }
   else{
+    response.send(flowers);
+  }
+});
+
+// GET /flowers/id
+router.get('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
     response.send(flower);
   }
 });
 
 // POST /flowers
-router.post('/',function(request, response){
+router.post('/', function(request, response) {
   const flower = request.body;
-  if(flowers.find(f => f.id === flowers.id)){
-    response.status(400).send('Duplicate id');
-  }
-  if(!flower.id){
-    response.status(400).send('No id');
-
-  }else {
+  if (!flower.id) {
+    response.status(400).send('Missing ID');
+  } else if (flowers.find(f => f.id === flower.id)) {
+    response.status(400).send('Duplicate ID');
+  } else {
     flowers.push(flower);
     response.status(201).send(flowers);
   }
 });
 
-
 // DELETE /flowers/id
-router.delete('/:id',function(request, response, next){
-  const flower = flowers.find(f => f.id === flower.id)
-  if(!flower){
-    next();
-  }
-  else{
+router.delete('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
+  } else {
     flowers = flowers.filter(f => f.id !== flower.id);
     response.status(200).send(flowers);
   }
 });
 
-
 // PUT /flowers/id
-router.put('/:id',function(request, response, next){
-  const flower = flowers.find(f => f.id === flower.id)
-  if(!flower){
-    next();
+router.put('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // Leads to 404
   } else {
-    if(request.body.color) flower.color = request.body.color;
-    if(request.body.season) flower.season = request.body.season;
+    if (request.body.color) flower.color = request.body.color;
+    if (request.body.season) flower.season = request.body.season;
     response.status(200).send(flowers);
-
   }
-
 });
+
 module.exports = router;
