9b6ccd32151f145c77d1c1c91e4b9d0d4c81f518
Isaac Brinkman <ibrin16@stlawu.edu>
2020-03-11 13:24:19 -0400

    Why wont this work

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -3,4 +3,15 @@ const express = require('express');
 
 // Create the router
 const router = express.Router();
+
+// GET /flowers
+
+// GET /flowers/id
+
+// POST /flowers
+
+// DELETE /flowers/id
+
+// PUT /flowers/id
+
 module.exports = router;
