cd3f37b23f6ff0f65ed98326234d57fe22f9d36a
Isaac Brinkman <ibrin16@stlawu.edu>
2020-03-11 13:48:40 -0400

    REST API for AJAX example

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -16,21 +16,55 @@ router.get('/', function(request, response){
 });
 // GET /flowers/id
 router.get('/:id', function(request, response, next){
-  const fl = flowers.find(f => f.id === request.params.id);
+  const flower = flowers.find(f => f.id === request.params.id);
   if(!flower){
     next();
   }
   else{
-    response.send(fl);
+    response.send(flower);
   }
 });
 
 // POST /flowers
+router.post('/',function(request, response){
+  const flower = request.body;
+  if(flowers.find(f => f.id === flowers.id)){
+    response.status(400).send('Duplicate id');
+  }
+  if(!flowers.id){
+    response.status(400).send('No id');
 
+  }else {
+    flowers.push(flower);
+    response.status(201).send(flowers);
+  }
+});
 
 
 // DELETE /flowers/id
+router.delete('/:id',function(request, response, next){
+  const flower = flowers.find(f => f.id === flowers.id)
+  if(!flower){
+    next();
+  }
+  else{
+    flowers = flowers.filter(f => f.id !== flower.id);
+    response.status(200).send(flowers);
+  }
+});
+
 
 // PUT /flowers/id
+router.put('/:id',function(request, response, next){
+  const flower = flowers.find(f => f.id === flowers.id)
+  if(!flower){
+    next();
+  } else {
+    if(request.body.color) flower.color = request.body.color;
+    if(request.body.season) flower.season = request.body.season;
+    response.status(200).send(flowers);
 
+  }
+
+});
 module.exports = router;
