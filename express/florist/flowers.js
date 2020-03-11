341bbd6f631d5c66c158dedc5c88e6140af20fec
Isaac Brinkman <ibrin16@stlawu.edu>
2020-03-11 13:30:07 -0400

    wont work

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -4,11 +4,30 @@ const express = require('express');
 // Create the router
 const router = express.Router();
 
-// GET /flowers
+// Pretend database
+let flowers = [
+  {id: 'tulip', color: 'pink', season:'spring'},
+  {id: 'rose', color: 'red', season:'summer'},
+];
 
+// GET /flowers
+router.get('/', function(request, response){
+  response.send(flowers);
+});
 // GET /flowers/id
+router.get('/:id', function(request, response, next){
+  const fl = flowers.find(f => f.id === request.params.id);
+  if(!flower){
+    next();
+  }
+  else{
+    response.send(fl);
+  }
+});
 
 // POST /flowers
+
+
 
 // DELETE /flowers/id
 
